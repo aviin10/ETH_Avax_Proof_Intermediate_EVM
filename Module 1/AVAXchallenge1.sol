06ca1bc4fafb468983cbf31cd8f7e4dd3adffb1e
aviin10 <avinkumar013@gmail.com>
2024-06-28 14:23:32 +0530

    Init Commit

---
+++ Module 1/AVAXchallenge1.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26; //solidity version
+contract Adminonly {
+    address public admin;
+    string private code;
+
+    modifier Admin() {
+        require(msg.sender == admin, "Only admin can access this.");
+        _;
+    }
+    constructor () {
+        admin = msg.sender;
+    }
+    function setcode (string memory _code) public Admin{
+        code = _code;
+    }
+    function getcode() public view returns (string memory){
+        return code;
+    }
+    function changeAdmin(address newAdmin)public Admin{
+        admin = newAdmin;
+    }
+}
+   
